671b7b40ab4f162ef412d979f3fe745993ce29b6
Samad <Samad.o@outlook.com>
2025-12-24 23:27:04 -0500

    Final commit for the night. Just some quick tweaks.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let unOrdered = document.getElementById("task-preview");
 
-document.getElementById("plus").addEventListener("click", function () {
-  let list = document.createElement("li");
 
-  list.setAttribute("class", "list-style")
+
+
+document.getElementById("plus").addEventListener("click", function () {
+  let list = document.createElement("li"); //Create list element.
+
+  list.setAttribute("class", "list-style");
+  //Give it a class.
 
   const addTask = document.getElementById("fadd-task").value;
-  list.innerHTML = addTask;
+  list.innerHTML = addTask; //This code grabs the value given in the input field
 
   if (addTask) {
     unOrdered.append(list);
   } else {
     alert("Please input a task!");
+    /* This will then append the created list and fill it with content.
+    If input field is left empty, then an alert will pop up.
+     */
   }
 });
