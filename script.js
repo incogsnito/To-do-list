c9b780705f4761da49ab6fb2503c48333be34c8b
Samad <Samad.o@outlook.com>
2025-12-25 08:22:55 -0500

    project nearing completion. Last thing I want to add is a remove task button

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let unOrdered = document.getElementById("task-preview");
-
-
-
+const unOrdered = document.getElementById("task-preview");
 
 document.getElementById("plus").addEventListener("click", function () {
-  let list = document.createElement("li"); //Create list element.
-
+  let list = document.createElement("li"); //Create necessary elements.
   list.setAttribute("class", "list-style");
-  //Give it a class.
+  //Give elements a class.
 
   const addTask = document.getElementById("fadd-task").value;
-  list.innerHTML = addTask; //This code grabs the value given in the input field
+  list.textContent = addTask;
+
+  //This code grabs the value given in the input field and puts it into a list.
 
   if (addTask) {
     unOrdered.append(list);
   } else {
     alert("Please input a task!");
+
     /* This will then append the created list and fill it with content.
     If input field is left empty, then an alert will pop up.
      */
   }
 });
+
+
